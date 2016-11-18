69f95d84f02aa3a3d81c445fd8c4b8b66361e3d8
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-11-18 14:29:15 +0300

    Handle frames via request queue

--- src/main.c
+++ src/main.c
@@ -5,9 +5,20 @@
 #include <event_loop.h>
 #include <v4l2_device.h>
 
+static void handle_complete(struct frame_request* req) {
+	struct v4l2_device* v4l2_device = req->data;
+
+	printf("complete %p %zu %p\n", req, req->count, req->complete);
+	if (req->count == 0) {
+		req->count = 10;
+		v4l2_device_add_frame_request(v4l2_device, req);
+	}
+}
+
 int main(int argc, char** argv) {
 	struct event_loop* event_loop;
 	struct v4l2_device* v4l2_device;
+	struct frame_request req;
 
 	event_loop = init_event_loop();
 	if (event_loop == NULL) {
@@ -25,6 +36,11 @@ int main(int argc, char** argv) {
 		perror("v4l2_device_attach_event_loop");
 		return 1;
 	}
+
+	req.complete = &handle_complete;
+	req.data = v4l2_device;
+	req.count = 10;
+	v4l2_device_add_frame_request(v4l2_device, &req);
 
 	while (!event_loop_wait(event_loop, 1000));
 
