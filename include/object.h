abd3e0f8935773ff7de541241ea425578bfb2d80
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2016-11-22 14:44:06 +0300

    Use correct ordering constraints

--- include/object.h
+++ include/object.h
@@ -10,20 +10,20 @@ struct object {
 
 static inline void init_object(struct object* obj, void (*defer)(struct object*)) {
 	obj->defer = defer;
-	AO_store_full(&obj->ref_count, 1);
+	AO_store_release(&obj->ref_count, 1);
 }
 
 static inline int object_get(struct object* obj) {
 	AO_t ref_count;
 
-	while ((ref_count = AO_load(&obj->ref_count)) &&
+	while ((ref_count = AO_load_acquire(&obj->ref_count)) &&
 	       !AO_compare_and_swap(&obj->ref_count, ref_count, ref_count+1));
 
 	return ref_count;
 }
 
 static inline void object_put(struct object* obj) {
-	if (AO_fetch_and_sub1(&obj->ref_count) == 1 && obj->defer)
+	if (AO_fetch_and_sub1_release(&obj->ref_count) == 1 && obj->defer)
 		obj->defer(obj);
 }
 
